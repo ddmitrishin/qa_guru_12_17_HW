9fe2e20aa4f665a390cbd4e983cb81444f357cf9
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-06-19 17:37:41 +0500

    add tests

---
+++ src/test/java/tests/ReqresinTests.java
@@ -0,0 +1,152 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresinTests {
+
+    @Test
+    void successfulLoginTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unSuccessfulLoginTest() {
+        String body = "{ \"email\": \"peter@klaven\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void successfulRegisterTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4), "token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unSuccessfulRegisterTest() {
+        String body = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void listTest() {
+
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://reqres.in/api/unknown")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.id", hasItems(4, 6));
+    }
+
+    @Test
+    void singleUserNotFoundTest() {
+
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    void createTest() {
+        String body = "{ \"name\": \"dima\", \"job\": \"qa\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("dima"),
+                        "job", is("qa"));
+    }
+
+    @Test
+    void updateTest() {
+        String body = "{ \"name\": \"morpheus\", \"job\": \"zion resident\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .put("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("name", is("morpheus"),
+                        "job", is("zion resident"));
+    }
+
+}
